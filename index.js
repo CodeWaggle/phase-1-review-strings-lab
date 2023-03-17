12fe61804e2d478c53af3d4d9ebec221e72bf45f
Joseph Schmitt <codewaggle@gmail.com>
2023-03-17 11:37:30 -0400

    phase1 strings

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+let welcomeMessage = `Welcome to Flatbook, ` + currentUser + "!";
+
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
